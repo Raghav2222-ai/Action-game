25bc6c21f6af566c3758319a8efcb6a702033086
Raghav2222-ai <119906489+Raghav2222-ai@users.noreply.github.com>
2023-01-30 14:52:40 +0530

    Add files via upload
    An action game

---
+++ script.js
@@ -0,0 +1,91 @@
+score = 0;
+cross = true;
+// if()
+// hiscoreval=0;
+// hiscoreBox=document.querySelector(".hiscoreBox")
+// // hiscoreBox.innerText ="Hiscore: "+hiscoreval;
+audio = new Audio('music.mp3');
+audiogo = new Audio('gameover.mp3');
+setTimeout(() => {
+    audio.play()
+}, 1000);
+document.onkeydown = function (e) {
+    console.log("Key code is: ", e.keyCode)
+    if (e.keyCode == 38) {
+        dino = document.querySelector('.dino');
+        dino.classList.add('animateDino');
+        setTimeout(() => {
+            dino.classList.remove('animateDino')
+        }, 700);
+    }
+    if (e.keyCode == 39) {
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = dinoX + 112 + "px";
+    }
+    if (e.keyCode == 37) {
+        dino = document.querySelector('.dino');
+        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+        dino.style.left = (dinoX - 112) + "px";
+    }
+}
+
+setInterval(() => {
+    dino = document.querySelector('.dino');
+    gameOver = document.querySelector('.gameOver');
+    obstacle = document.querySelector('.obstacle');
+
+    dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
+    dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue('top'));
+
+    ox = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
+    oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('top'));
+
+    offsetX = Math.abs(dx - ox);
+    offsetY = Math.abs(dy - oy);
+    // console.log(offsetX, offsetY)
+    if (offsetX < 73 && offsetY < 52) {
+        gameOver.innerText = "Game Over - Reload to Play Again"
+        obstacle.classList.remove('obstacleAni')
+        audiogo.play();
+        setTimeout(() => {
+            audiogo.pause();
+            audio.pause();
+        }, 1000);
+    }
+    else if (offsetX < 145 && cross) {
+        score += 1;
+//         hiscoreval=score;
+        updateScore(score);
+//         if(score>hiscoreval){
+//             hiscoreval = score;
+//             localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
+//             hiscoreBox.innerText += hiscoreval;
+//         }
+//         let hiscore = localStorage.getItem("hiscore");
+// if(hiscore === null){
+//     hiscoreval = 0;
+//     localStorage.setItem("hiscore", JSON.stringify(hiscoreval))
+// }
+// else{
+//     hiscoreval = JSON.parse(hiscore);
+//     hiscoreBox.innerText = "HiScore: " + hiscore;
+// }
+        cross = false;
+        setTimeout(() => {
+            cross = true;
+        }, 1000);
+        setTimeout(() => {
+            aniDur = parseFloat(window.getComputedStyle(obstacle, null).getPropertyValue('animation-duration'));
+            newDur = aniDur - 0.1;
+            obstacle.style.animationDuration = newDur + 's';
+            console.log('New animation duration: ', newDur)
+        }, 500);
+
+    }
+
+}, 10);
+
+function updateScore(score) {
+    scoreCont.innerText = "Your Score: " + score
+}
